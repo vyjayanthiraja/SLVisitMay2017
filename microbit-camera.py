d75437ab68e3892cf995004d2944e092f2e11832
Vyjayanthi Raja <vyjayanthi.raja@gmail.com>
2017-05-11 00:30:17 -0700

    Couple more fixes to microbit_camera.py

--- microbit-camera.py
+++ microbit-camera.py
@@ -1,14 +1,18 @@
 from picamera import PiCamera
 from gpiozero import Button
 from time import sleep
-from image_ai import getEmotion
+from image_ai import *
+from utils import *
 
+imageFileName = 'photo.jpg'
 button = Button(4, pull_up=False)
 with PiCamera() as camera:
+    sleep(5)
     while True:
-        sleep(5)
         button.wait_for_press()
-        imageFileName = 'selfie.jpg'
+        camera.image_effect = 'colorswap'
         camera.capture(imageFileName)
-        
+        subject = getDescription(imageFileName)
+        sendMail('vyjayanthi.raja@gmail.com', subject, imageFileName)
+        print("Finished sending email")
 
